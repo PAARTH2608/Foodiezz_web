b8fc12696ebbcb58642887dd6c9b47815dbf8caa
PAARTH2608 <76266935+PAARTH2608@users.noreply.github.com>
2021-07-22 15:15:27 +0530

    error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,12 +2,19 @@ import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import { useEffect, useState } from 'react';
-
+// we cannot use async func as a function in useEffect rather we created another func inside the use effect func which can be made async
 const AvailableMeals = () => {
-    const [meals, setMeals] = useState([])
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState()
+    // getting DUMMY_MEALS from backend.
     useEffect(() => {
+        // loader.
         const fetchMeals = async () => {
-            const response = await fetch('https://react-http-f5ae6-default-rtdb.firebaseio.com/meals.json')
+            const response = await fetch('https://react-http-f5ae6-default-rtdb.firebaseio.com/meals.json');
+            if(!response.ok){
+                throw new Error('Something went wrong!');
+            }
             const reponseData = await response.json();
             const loadedMeals = [];
             for( const key in reponseData){
@@ -19,11 +26,25 @@ const AvailableMeals = () => {
                 })
             }
             setMeals(loadedMeals);
+            setIsLoading(false);
         };
-        fetchMeals();
-        
+        fetchMeals().catch(error => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });  
     }, []);
 
+    if(isLoading){
+        return <section className={classes.MealsLoading}>
+            <p>Loading...</p>
+        </section>
+    }
+
+    if(httpError){
+        return <section className={classes.MealsError}>
+            <p>{httpError}</p>
+        </section>
+    }
     // const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
     const mealsList = meals.map(meal => (
     <MealItem 
@@ -34,10 +55,12 @@ const AvailableMeals = () => {
         price={meal.price}
     />)
     );
-    return <section className={classes.meals}>
-        <Card>
-            <ul>{mealsList}</ul>
-        </Card>
-    </section>
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
+        </section>
+        )
 };
 export default AvailableMeals;
