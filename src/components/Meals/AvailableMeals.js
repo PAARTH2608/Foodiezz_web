bef9fd6dbab79ea37c2e87ade925f09c48b95d6c
PAARTH2608 <76266935+PAARTH2608@users.noreply.github.com>
2021-07-22 13:39:45 +0530

    firebase added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,32 @@
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
-
+import { useEffect, useState } from 'react';
+// we cannot use async func as a function in useEffect rather we created another func inside the use effect func which can be made async
 const AvailableMeals = () => {
+    const [meals, setMeals] = useState([])
+    // getting DUMMY_MEALS from backend.
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-http-f5ae6-default-rtdb.firebaseio.com/meals.json')
+            const reponseData = await response.json();
+            const loadedMeals = [];
+            for( const key in reponseData){
+                loadedMeals.push({
+                    id: key,
+                    name: reponseData[key].name,
+                    description: reponseData[key].description,
+                    price: reponseData[key].price,
+                })
+            }
+            setMeals(loadedMeals);
+        };
+        fetchMeals();
+        
+    }, []);
+
     // const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
-    const mealsList = DUMMY_MEALS.map(meal => (
+    const mealsList = meals.map(meal => (
     <MealItem 
         key={meal.id}
         id= {meal.id} 
